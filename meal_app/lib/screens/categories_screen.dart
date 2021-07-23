de6d30a30e086b4724b18217d093592d70a12d97
lina-jamal <linahjamal89@gmail.com>
2021-07-23 22:06:49 +0300

    wrap categorScreen with Scaffold

--- meal_app/lib/screens/categories_screen.dart
+++ meal_app/lib/screens/categories_screen.dart
@@ -5,16 +5,19 @@ import '../utlis/dummy_data.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-        padding: EdgeInsets.all(25),
-        children: DUMMY_CATEGORIES
-            .map((catData) =>
-                CategortItem(catData.id, catData.title, catData.color))
-            .toList(),
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-            maxCrossAxisExtent: 200,
-            childAspectRatio: 3 / 2,
-            crossAxisSpacing: 20,
-            mainAxisSpacing: 20));
+    return Scaffold(
+      appBar: AppBar(title: Text('Meal')),
+      body: GridView(
+          padding: EdgeInsets.all(25),
+          children: DUMMY_CATEGORIES
+              .map((catData) =>
+                  CategortItem(catData.id, catData.title, catData.color))
+              .toList(),
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 200,
+              childAspectRatio: 3 / 2,
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 20)),
+    );
   }
 }
