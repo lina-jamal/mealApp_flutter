611d1ec3ec31ae34dd0c1b5d21e913db0203bcf9
lina-jamal <linahjamal89@gmail.com>
2021-07-23 23:43:14 +0300

    create selectCategory function to push route

--- meal_app/lib/widget/category_item.dart
+++ meal_app/lib/widget/category_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meal_app/screens/category_meal_screen.dart';
 
 class CategortItem extends StatelessWidget {
   final String id;
@@ -6,10 +7,18 @@ class CategortItem extends StatelessWidget {
   final Color color;
 
   CategortItem(this.id, this.title, this.color);
+
+  void selectCategory(BuildContext ctx) {
+    Navigator.of(ctx).pushNamed(CategoryMealScreen.routeName,
+        arguments: {'id': id, 'title': title});
+  }
+
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () => {},
+      onTap: () => selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
       child: Container(
         padding: EdgeInsets.all(15),
         child: Text(
