d89d8304265291361e4d6cdcfd8b6b99a2fd6ab2
lina-jamal <linahjamal89@gmail.com>
2021-07-24 00:06:24 +0300

    filter meals by category and return it in listview

--- meal_app/lib/screens/category_meal_screen.dart
+++ meal_app/lib/screens/category_meal_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meal_app/utlis/dummy_data.dart';
 
 class CategoryMealScreen extends StatefulWidget {
   static const routeName = 'categoryMeals';
@@ -13,11 +14,19 @@ class _CategoryMealScreenState extends State<CategoryMealScreen> {
         ModalRoute.of(context)!.settings.arguments as Map<String, String>;
     final catId = routArg['id'];
     final catTitle = routArg['title'];
+    final catMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(catId);
+    }).toList();
 
     return Scaffold(
         appBar: AppBar(
           title: Text(catTitle!),
         ),
-        body: null);
+        body: ListView.builder(
+          itemBuilder: (ctx, index) {
+            return Text(catMeals[index].title);
+          },
+          itemCount: catMeals.length,
+        ));
   }
 }
