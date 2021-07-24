07a0f1eba897a520bcd910ad1d6f28b6631764a4
lina-jamal <linahjamal89@gmail.com>
2021-07-24 18:09:42 +0300

    seprate home page from main.dart file

---
+++ meal_app/lib/screens/home_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({required this.title});
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Meal App'),
+      ),
+      body: null,
+    );
+  }
+}
