c1d29158d65f117b7b47eb2ef51cefe99b5cb6f3
lina-jamal <linahjamal89@gmail.com>
2021-07-24 05:48:44 +0300

    create tabs screen and navigate the pages inside tab

---
+++ meal_app/lib/screens/tabs_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'categories_screen.dart';
+import 'favorites_screen.dart';
+
+class TabsScreen extends StatefulWidget {
+  @override
+  _TabsScreenState createState() => _TabsScreenState();
+}
+
+final List<Map<String, dynamic>> _pages = [
+  {'page': CategoriesScreen(), 'title': 'Categories'},
+  {'page': FavouritesScreen(), 'title': 'your Favourites'},
+];
+
+class _TabsScreenState extends State<TabsScreen> {
+  int _selectedPageIndex = 0;
+  void _selectPage(int val) {
+    setState(() {
+      _selectedPageIndex = val;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(_pages[_selectedPageIndex]['title'])),
+      body: (_pages[_selectedPageIndex]['page']),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        backgroundColor: Theme.of(context).primaryColor,
+        selectedItemColor: Theme.of(context).accentColor,
+        unselectedItemColor: Colors.white,
+        currentIndex: _selectedPageIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            label: 'Categories',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: 'Favourites',
+          ),
+        ],
+      ),
+    );
+  }
+}
