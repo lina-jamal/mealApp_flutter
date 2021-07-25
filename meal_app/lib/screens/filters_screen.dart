1311c8dc749f7015ce754becee2f10a1abcd071f
lina-jamal <linahjamal89@gmail.com>
2021-07-25 21:17:55 +0300

    wrap fillter screen with a Scaffold

--- meal_app/lib/screens/filters_screen.dart
+++ meal_app/lib/screens/filters_screen.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
+import '../widget/main_drawer.dart';
 
 class FiltersScreen extends StatelessWidget {
   static const routeName = '/filters';
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Filters'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('your filter'),
+      ),
+      body: Center(
+        child: Text('Filters'),
+      ),
+      drawer: MainDrawer(),
     );
   }
 }
