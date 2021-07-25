5a4973c3354c766f547776cbfd9537f456cfe17f
lina-jamal <linahjamal89@gmail.com>
2021-07-26 01:37:23 +0300

    create switchSwitchListTile in filter screen

--- meal_app/lib/screens/filters_screen.dart
+++ meal_app/lib/screens/filters_screen.dart
@@ -9,14 +9,41 @@ class FiltersScreen extends StatefulWidget {
 }
 
 class _FiltersScreenState extends State<FiltersScreen> {
+  bool _glutenFree = false;
+  bool _lactoseFree = false;
+  bool _vegan = false;
+  bool _vegetarian = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('your filter'),
       ),
-      body: Center(
-        child: Text('Filters'),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              'Adjust your meal selection',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+          ),
+          Expanded(
+              child: ListView(
+            children: [
+              SwitchListTile(
+                title: Text('Gluten Free'),
+                value: _glutenFree,
+                subtitle: Text('Only include glutenfree meals'),
+                onChanged: (value) {
+                  setState(() {
+                    _glutenFree = value;
+                  });
+                },
+              )
+            ],
+          ))
+        ],
       ),
       drawer: MainDrawer(),
     );
