0afaf8fd6d234abc7b3ba9922943161bfc278ea3
lina-jamal <linahjamal89@gmail.com>
2021-07-26 00:20:14 +0300

    turn the filtr screen from stateless to statefull widget

--- meal_app/lib/screens/filters_screen.dart
+++ meal_app/lib/screens/filters_screen.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import '../widget/main_drawer.dart';
 
-class FiltersScreen extends StatelessWidget {
+class FiltersScreen extends StatefulWidget {
   static const routeName = '/filters';
+
+  @override
+  _FiltersScreenState createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
