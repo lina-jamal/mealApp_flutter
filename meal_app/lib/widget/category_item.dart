a745a92dbd431de86853aea84ba0dfc6a59354ae
lina-jamal <lina@cloud283.com>
2021-09-07 14:32:53 +0300

    wrap CategoriItem with MaterialApp to make test

--- meal_app/lib/widget/category_item.dart
+++ meal_app/lib/widget/category_item.dart
@@ -15,22 +15,26 @@ class CategortItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () => selectCategory(context),
-      splashColor: Theme.of(context).primaryColor,
-      borderRadius: BorderRadius.circular(15),
-      child: Container(
-        padding: EdgeInsets.all(15),
-        child: Text(
-          title,
-          style: Theme.of(context).textTheme.headline6,
+    return MaterialApp(
+      home: Material(
+        child: InkWell(
+          onTap: () => selectCategory(context),
+          splashColor: Theme.of(context).primaryColor,
+          borderRadius: BorderRadius.circular(15),
+          child: Container(
+            padding: EdgeInsets.all(15),
+            child: Text(
+              title,
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [color.withOpacity(0.4), color],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight),
+                borderRadius: BorderRadius.circular(15)),
+          ),
         ),
-        decoration: BoxDecoration(
-            gradient: LinearGradient(
-                colors: [color.withOpacity(0.4), color],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight),
-            borderRadius: BorderRadius.circular(15)),
       ),
     );
   }
